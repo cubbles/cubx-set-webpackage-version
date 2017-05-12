d8f29231d8ea84b652dc496263f48f7607f1808d
Edwin Gamboa <edwjov92@gmail.com>
2017-05-12 08:19:21 -0500

    CDT-56: Add bin version of the webpackage

---
+++ bin/cubx-set-webpackage-version.js
@@ -0,0 +1,21 @@
+'use strict';
+var WebpackageVersionSetter = require('../lib/cubx-set-webpackage-version');
+var commandLineArgs = require('command-line-args');
+
+var optionDefinitions = [
+  { name: 'webpackagePath', type: String, alias: 'p' },
+  { name: 'version', type: String, alias: 'v' }
+];
+
+var options = commandLineArgs(optionDefinitions);
+
+if (!options.webpackagePath) {
+  console.error('Missed necessary parameter "webpackagePath". Usage: cubx-prepare-webpackage-release -p <webpackagPath>');
+  process.exit(0);
+}
+if (!options.version) {
+  console.error('Missed necessary parameter "version". Usage: cubx-prepare-webpackage-release -v <version>');
+  process.exit(0);
+}
+var setter = new WebpackageVersionSetter(options.webpackagePath, options.version);
+setter.setManifestVersion();
