9c3ed973ca2645eb9f5981592fd6e823d74da402
edwingamboa <edwingamboa355@gmail.com>
2018-11-20 17:23:30 -0500

    #1: Make script avialable for linux. Rename parameter (-v version cannot be used)

--- bin/cubx-set-webpackage-version.js
+++ bin/cubx-set-webpackage-version.js
@@ -1,21 +1,22 @@
+#! /usr/bin/env node
 'use strict';
 var WebpackageVersionSetter = require('../lib/cubx-set-webpackage-version');
 var commandLineArgs = require('command-line-args');
 
 var optionDefinitions = [
   { name: 'webpackagePath', type: String, alias: 'p' },
-  { name: 'version', type: String, alias: 'v' }
+  { name: 'webpackageVersion', type: String, alias: 'V' }
 ];
 
 var options = commandLineArgs(optionDefinitions);
 
 if (!options.webpackagePath) {
-  console.error('Missed necessary parameter "webpackagePath". Usage: cubx-prepare-webpackage-release -p <webpackagPath> -v <version>');
+  console.error('Missed necessary parameter "webpackagePath". Usage: cubx-prepare-webpackage-release -p <webpackagPath> -V <webpackageVersion>');
   process.exit(0);
 }
-if (!options.version) {
-  console.error('Missed necessary parameter "version". Usage: cubx-prepare-webpackage-release -p <webpackagPath> -v <version>');
+if (!options.webpackageVersion) {
+  console.error('Missed necessary parameter "webpackageVersion". Usage: cubx-prepare-webpackage-release -p <webpackagPath> -V <webpackageVersion>');
   process.exit(0);
 }
-var setter = new WebpackageVersionSetter(options.webpackagePath, options.version);
+var setter = new WebpackageVersionSetter(options.webpackagePath, options.webpackageVersion);
 setter.setManifestVersion();
